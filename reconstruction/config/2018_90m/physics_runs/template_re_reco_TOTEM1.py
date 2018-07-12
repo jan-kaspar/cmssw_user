40751d2cf551d85a54acc8d38fd54c54658f0eb7
Jan Kašpar <jan.kaspar@cern.ch>
2018-07-12 10:42:41 +0200

    Configs form 2018 90m physics fills.

---
+++ reconstruction/config/2018_90m/physics_runs/template_re_reco_TOTEM1.py
@@ -0,0 +1,100 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("ReRecoWithAlignment", eras.ctpps_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
+)
+
+# pixel mappings
+process.load("CondFormats.CTPPSReadoutObjects.CTPPSPixelDAQMappingESSourceXML_cfi")
+
+# data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+$input_files
+  ),
+
+  lumisToProcess = cms.untracked.VLuminosityBlockRange($ls_selection),
+
+  dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+  inputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep TotemRPRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSPixelRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSDiamondRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSDiamondLocalTrackedmDetSetVector_*_*_*',
+    'keep edmTriggerResults_*_*_*',
+    'keep GlobalAlgBlkBXVector_*_*_*'
+    #'keep CTPPSLocalTrackLites_*_*_*',
+  )
+)
+
+#process.maxEvents = cms.untracked.PSet(
+#  input = cms.untracked.int32(100)
+#)
+
+# define global tag
+process.GlobalTag.globaltag = '101X_dataRun2_HLT_v7'
+
+# RP reconstruction chain with standard settings
+process.load("RecoCTPPS.Configuration.recoCTPPS_DD_cff")
+
+# use the correct geometry
+del(process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles[-1])
+process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles.append("$geometry")
+
+# add alignment corrections
+process.ctppsIncludeAlignmentsFromXML.RealFiles += cms.vstring($alignment_files)
+
+#process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+
+# reconstruction sequences
+process.stripReProcessing = cms.Sequence(
+  process.totemRPUVPatternFinder
+  * process.totemRPLocalTrackFitter
+)
+
+process.pixelReProcessing = cms.Sequence(
+  process.ctppsPixelLocalTracks
+)
+
+process.path_reco = cms.Path(
+  #process.dump *
+  process.stripReProcessing
+  * process.pixelReProcessing
+  * process.ctppsLocalTrackLiteProducer
+  #* process.dump
+)
+
+# filter
+process.lv1BitFilter = cms.EDFilter("L1BitFilter",
+    lv1Bits = cms.vuint32($lv1Bits),
+    verbosity = cms.untracked.uint32(0)
+)
+
+process.path_filter = cms.Path(
+  process.lv1BitFilter
+)
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("$output_file"),
+  SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('path_filter')
+  )
+)
+
+process.path_output = cms.EndPath(process.output)
+
+# schedule
+process.schedule = cms.Schedule(process.path_reco, process.path_filter, process.path_output)
