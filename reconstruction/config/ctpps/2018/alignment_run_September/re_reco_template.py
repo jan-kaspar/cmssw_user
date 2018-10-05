1eb6977eee2ce376c71939b99ccea23f677c18a7
Jan Kašpar <jan.kaspar@cern.ch>
2018-10-05 13:42:54 +0200

    Added configs.

---
+++ reconstruction/config/ctpps/2018/alignment_run_September/re_reco_template.py
@@ -0,0 +1,87 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("CTPPSReRecoWithAlignment", eras.ctpps_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
+)
+
+# pixel mappings
+process.load("CondFormats.CTPPSReadoutObjects.CTPPSPixelDAQMappingESSourceXML_cfi")
+
+# data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+$input_files
+  ),
+
+  lumisToProcess = cms.untracked.VLuminosityBlockRange($ls_selection),
+
+  inputCommands = cms.untracked.vstring(
+    'keep *',
+    'drop TotemRPUVPatternedmDetSetVector_*_*_*',
+    'drop TotemRPLocalTrackedmDetSetVector_*_*_*',
+    'drop CTPPSPixelLocalTrackedmDetSetVector_*_*_*',
+    'drop CTPPSLocalTrackLites_*_*_*'
+  )
+)
+
+#process.maxEvents = cms.untracked.PSet(
+#  input = cms.untracked.int32(100)
+#)
+
+# define global tag
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_ReReco_EOY17_v2', '')
+
+# load reco sequences
+process.load("RecoCTPPS.Configuration.recoCTPPS_DD_cff")
+
+# use the correct geometry
+del(process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles[-1])
+process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles.append("$geometry")
+
+# add alignment corrections
+process.ctppsIncludeAlignmentsFromXML.RealFiles += cms.vstring($alignment_files)
+
+#process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+
+# reconstruction sequences
+process.stripReProcessing = cms.Sequence(
+  process.totemRPUVPatternFinder
+  * process.totemRPLocalTrackFitter
+)
+
+process.pixelReProcessing = cms.Sequence(
+  process.ctppsPixelLocalTracks
+)
+
+process.p = cms.Path(
+  process.stripReProcessing
+  * process.pixelReProcessing
+  * process.ctppsLocalTrackLiteProducer
+  #* process.dump
+)
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("$output_file"),
+  outputCommands = cms.untracked.vstring(
+    "drop *",
+    'keep TotemRPUVPatternedmDetSetVector_*_*_*',
+    'keep CTPPSDiamondRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSPixelRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSLocalTrackLites_*_*_*'
+  )
+)
+
+process.outpath = cms.EndPath(process.output)
