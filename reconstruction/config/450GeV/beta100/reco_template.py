8b565659255b666223f10724c290e2fe0e177aeb
Jan Kašpar <jan.kaspar@cern.ch>
2018-10-15 15:41:39 +0200

    Real data at 900GeV.

---
+++ reconstruction/config/450GeV/beta100/reco_template.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process("CTPPSReconstructionChainTest", eras.ctpps_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
+)
+
+# raw data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+$input_files
+  ),
+
+  lumisToProcess = cms.untracked.VLuminosityBlockRange($ls_selection),
+
+  inputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep FEDRawDataCollection_*_*_*'
+  )
+)
+
+#process.maxEvents = cms.untracked.PSet(
+#  input = cms.untracked.int32(1000)
+#)
+
+# raw-to-digi conversion
+process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
+
+# RP reconstruction chain with standard settings
+process.load("RecoCTPPS.Configuration.recoCTPPS_DD_cff")
+
+process.p = cms.Path(
+  process.ctppsRawToDigi *
+  process.recoCTPPS
+)
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("$output_file")
+)
+
+process.outpath = cms.EndPath(process.output)
