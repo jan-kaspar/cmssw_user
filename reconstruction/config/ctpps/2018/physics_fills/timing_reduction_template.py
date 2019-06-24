3596b3eaefdccfad9b64259e0d1df5f8c3749864
Jan Kašpar <jan.kaspar@cern.ch>
2019-06-24 14:51:10 +0200

    Scripts added.

---
+++ reconstruction/config/ctpps/2018/physics_fills/timing_reduction_template.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process("reduction", eras.ctpps_2016)
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
+)
+
+# data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(),
+  skipBadFiles = cms.untracked.bool(True),
+  duplicateCheckMode = cms.untracked.string("checkAllFilesOpened"),
+  dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+
+  inputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep CTPPSDiamondRecHitedmDetSetVector_*_*_*',
+    'keep CTPPSDiamondLocalTrackedmDetSetVector_*_*_*',
+    'keep CTPPSLocalTrackLites_*_*_*'
+  )
+)
+$input_file_commands
+
+#process.maxEvents = cms.untracked.PSet(
+#  input = cms.untracked.int32(100)
+#)
+
+# apply JSON file
+import FWCore.PythonUtilities.LumiList as LumiList
+import FWCore.ParameterSet.Types as CfgTypes
+process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+JSONfile = '$ls_selection'
+myLumis = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
+process.source.lumisToProcess.extend(myLumis)
+
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("$output_file")
+)
+
+process.outpath = cms.EndPath(process.output)
